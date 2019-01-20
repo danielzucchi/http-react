af804a8f4f72af506a3cee7d4d7d3de60ebcd8c1
Daniel Zucchi <danielzucchi@Daniels-MacBook-Pro.local>
2019-01-20 17:50:58 +0000

    Axios call rendering posts

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -6,22 +6,45 @@ import NewPost from "../../components/NewPost/NewPost";
 import "./Blog.css";
 
 class Blog extends Component {
+  state = {
+    posts: [],
+    selectedPostId: null
+  };
+
   componentDidMount() {
     axios.get("https://jsonplaceholder.typicode.com/posts").then(res => {
-      console.log(res.data);
+      const posts = res.data.slice(0, 4);
+      const updatedPosts = posts.map(post => {
+        return {
+          ...post,
+          author: "Daniel"
+        };
+      });
+      this.setState({ posts: updatedPosts });
     });
   }
 
+  postSelectedHandler = id => {
+    this.setState({ selectedPostId: id });
+  };
+
   render() {
+    const posts = this.state.posts.map(post => {
+      return (
+        <Post
+          title={post.title}
+          author={post.author}
+          key={post.id}
+          clicked={() => this.postSelectedHandler(post.id)}
+        />
+      );
+    });
+
     return (
       <div>
-        <section className="Posts">
-          <Post />
-          <Post />
-          <Post />
-        </section>
+        <section className="Posts">{posts}</section>
         <section>
-          <FullPost />
+          <FullPost id={this.state.selectedPostId} />
         </section>
         <section>
           <NewPost />
