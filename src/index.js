c777c71e0467a21ca6e4b06da807a3c112d9c0f5
Daniel Zucchi <danielzucchi@daniels-mbp.mynet>
2019-01-27 13:51:40 +0000

    Reservsed packaged changes. Add axios interceptors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import axios from "axios";
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+axios.interceptors.request.use(req => {
+  console.log(req);
+  return req;
+}),
+  err => {
+    console.log(err);
+    return Promise.reject(err);
+  };
+
+axios.interceptors.response.use(res => {
+  console.log(res);
+  return res;
+}),
+  err => {
+    console.log(err);
+    return Promise.reject(err);
+  };
+
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
