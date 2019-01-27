828830db6a08abafae38fee6e0074ea8bfa74772
Daniel Zucchi <danielzucchi@daniels-mbp.mynet>
2019-01-27 15:03:08 +0000

    Fix syntax error

--- src/index.js
+++ src/index.js
@@ -9,23 +9,27 @@ axios.defaults.baseURL = "https://jsonplaceholder.typicode.com";
 axios.defaults.headers.common["Authorization"] = "AUTH TOKEN";
 axios.defaults.headers.post["Content-Type"] = "aplication/json";
 
-axios.interceptors.request.use(req => {
-  console.log(req);
-  return req;
-}),
+axios.interceptors.request.use(
+  req => {
+    console.log(req);
+    return req;
+  },
   err => {
     console.log(err);
     return Promise.reject(err);
-  };
+  }
+);
 
-axios.interceptors.response.use(res => {
-  console.log(res);
-  return res;
-}),
+axios.interceptors.response.use(
+  res => {
+    console.log(res);
+    return res;
+  },
   err => {
     console.log(err);
     return Promise.reject(err);
-  };
+  }
+);
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
